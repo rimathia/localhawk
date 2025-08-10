7c9d227f5e8d87e045f4e9006688b06fefe6dcc2
rimathia <rimathia@users.noreply.github.com>
2025-08-10 16:21:39 +0200

    do not use scryfall rate-limiting for images, create an error log entry if we generate too many requests

--- magic-proxy-core/src/scryfall/client.rs
+++ magic-proxy-core/src/scryfall/client.rs
@@ -1,13 +1,13 @@
+use crate::error::ProxyError;
 use lazy_static::lazy_static;
 use log::debug;
+use reqwest::blocking::Request;
 use tokio::time::{Duration, Instant};
-use crate::error::ProxyError;
 
 // Headers required according to https://scryfall.com/docs/api/
 const USER_AGENT: &str = "magic-proxy-core/0.1";
 const ACCEPT: &str = "*/*";
 const SCRYFALL_COOLDOWN: Duration = Duration::from_millis(100);
-
 
 // Use a blocking mutex since we are only holding the lock to find out when we can call
 lazy_static! {
@@ -31,44 +31,57 @@ impl ScryfallClient {
             reqwest::header::ACCEPT,
             reqwest::header::HeaderValue::from_static(ACCEPT),
         );
-        
+
         let client = reqwest::Client::builder()
             .default_headers(headers)
             .build()?;
-            
+
         Ok(ScryfallClient { client })
     }
 
     pub async fn call(&self, uri: &str) -> Result<reqwest::Response, ProxyError> {
-        let next_call = {
-            let mut l = *LAST_SCRYFALL_CALL.lock().unwrap();
-            l += SCRYFALL_COOLDOWN;
-            l
-        };
-        tokio::time::sleep_until(next_call).await;
-        debug!("calling scryfall API: {}", uri);
-        
+        if !uri.contains(".scryfall.io") {
+            let next_call = {
+                let mut l = *LAST_SCRYFALL_CALL.lock().unwrap();
+                l += SCRYFALL_COOLDOWN;
+                l
+            };
+            tokio::time::sleep_until(next_call).await;
+            debug!("calling scryfall API (rate-limited): {}", uri);
+        } else {
+            debug!("calling scryfall API (not rate-limited): {}", uri);
+        }
+
         match self.client.get(uri).send().await {
             Ok(response) => Ok(response),
-            Err(e) => Err(ProxyError::Network(e))
+            Err(e) => {
+                if e.status() == Some(reqwest::StatusCode::TOO_MANY_REQUESTS) {
+                    log::error!(
+                        "scryfall API has returned status code 429 (too many requests): {}",
+                        e
+                    );
+                }
+                Err(ProxyError::Network(e))
+            }
         }
     }
 
-    pub async fn get_image(&self, url: &str) -> Result<printpdf::image_crate::DynamicImage, ProxyError> {
+    pub async fn get_image(
+        &self,
+        url: &str,
+    ) -> Result<printpdf::image_crate::DynamicImage, ProxyError> {
         let raw_bytes = self.get_image_bytes(url).await?;
-        
+
         printpdf::image_crate::load_from_memory(&raw_bytes)
             .map_err(|e| ProxyError::Cache(format!("Failed to load image: {}", e)))
     }
-    
+
     /// Get raw image bytes from URL (uses same rate limiting as get_image)
     pub async fn get_image_bytes(&self, url: &str) -> Result<Vec<u8>, ProxyError> {
         let response = self.call(url).await?;
         let bytes = response.bytes().await?;
         Ok(bytes.to_vec())
     }
-
-
 }
 
 impl Default for ScryfallClient {
